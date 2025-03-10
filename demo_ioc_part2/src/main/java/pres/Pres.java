622540963a2ef98bbb5f01f4e8465e68f7371598
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-03-10 11:06:52 +0000

    injection des des dependances avec spring framework version xml

---
+++ demo_ioc_part2/src/main/java/pres/Pres.java
@@ -0,0 +1,15 @@
+package pres;
+
+import dao.DaoImpl;
+import metier.MetierImpl;
+
+import java.lang.reflect.Method;
+
+public class Pres {
+    public static void main(String[] args) {
+        DaoImpl dao = new DaoImpl();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println(metier.calcul());
+    }
+}
